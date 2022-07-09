0ed4f791e286251345e3f6b1c302b9950a438862
Shival-Gupta <sgupta.5545@gmail.com>
2022-07-09 19:25:35 +0530

    signUp Design

--- src/quizapp/QuizApp.java
+++ src/quizapp/QuizApp.java
@@ -14,7 +14,7 @@ public class QuizApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Login loginWindow = new Login();
+        LoginWindow loginWindow = new LoginWindow();
         loginWindow.setVisible(true);
     }
     
